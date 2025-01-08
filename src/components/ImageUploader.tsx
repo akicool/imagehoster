dc8b84d3fe0f5da94e6d9bb34b36582e65fa9fbf
akicool <liid05754@gmail.com>
2025-01-08 19:05:55 +0300

    feat: update ui

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,34 +1,102 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
-import { useDropzone } from "react-dropzone";
+import { FormEvent, memo, useCallback, useEffect, useState } from "react";
+import { type FileWithPath, useDropzone } from "react-dropzone";
 import { toast } from "react-toastify";
-import { InfoIcon, UploadIcon } from "lucide-react";
+import { FileIcon, InfoIcon, UploadIcon, XIcon } from "lucide-react";
 import clsx from "clsx";
 import { Inter } from "next/font/google";
+import { Popover } from "react-tiny-popover";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
+const FileSize = ({ size }: { size: number }) => {
+  const formatBytes = (bytes: number, decimals = 2) => {
+    if (bytes === 0) return "0 Bytes";
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  };
+
+  return <span className="text-xs text-gray-500">{formatBytes(size)}</span>;
+};
+
+const FileList = memo(
+  ({
+    files,
+    uploading,
+    handleRemoveFile,
+  }: {
+    files: FileWithPath[];
+    uploading: boolean;
+    handleRemoveFile: (file: FileWithPath) => void;
+  }) => {
+    return (
+      <div className="flex flex-col">
+        {files.map((file, idx) => (
+          <div
+            key={idx}
+            className="mb-2 p-2 border-solid border-[#E6E6E6] border-2 rounded-md flex justify-between items-center"
+          >
+            <div className="flex gap-2 items-center">
+              <FileIcon size={16} color="#ADACB0" />
+
+              <p
+                className={clsx(
+                  "text-sm text-gray-600 font-medium",
+                  inter.variable
+                )}
+              >
+                File {file.name}
+              </p>
+
+              <FileSize size={file.size} />
+            </div>
+
+            <div className="flex items-center gap-2">
+              {uploading && <div className="file-loader" />}
+
+              <button onClick={() => handleRemoveFile(file)}>
+                <XIcon size={16} color="#0A090B" />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+);
+
+FileList.displayName = "FileList";
+
 export function ImageUploader() {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
-  const [files, setFiles] = useState<Array<File> | null>([]);
+  const [files, setFiles] = useState<FileWithPath[] | File[]>([]);
   const [uploading, setUploading] = useState(false);
   const [viewUrl, setViewUrl] = useState<string | null>(null);
   const [isPrivate, setIsPrivate] = useState(false);
   const [info, setInfo] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!acceptedFiles || acceptedFiles.length === 0) return;
+    if (!files || files.length === 0) return;
+
+    if (files.length > 5) {
+      toast.error("Можно загрузить не более 5 файлов");
+      return;
+    }
 
     setUploading(true);
+
     const formData = new FormData();
-    acceptedFiles.map((file) => formData.append("files", file));
+    files.map((file) => formData.append("files", file));
     formData.append("isPrivate", isPrivate.toString());
     console.log({ formData });
 
@@ -40,7 +108,6 @@ export function ImageUploader() {
 
       const data = await response.json();
       console.log({ data });
-      
 
       if (data.success) {
         toast.success("Успешно загружено");
@@ -51,11 +118,24 @@ export function ImageUploader() {
       }
     } catch (error) {
       console.error("Error uploading file:", error);
-      toast("Ошибка");
+
+      toast.error(
+        error instanceof Error ? error.message : "Ошибка при загрузке"
+      );
     } finally {
       setUploading(false);
     }
   };
+
+  const handleRemoveFile = useCallback((file: FileWithPath) => {
+    setFiles((prevFiles) => prevFiles.filter((prevFile) => prevFile !== file));
+  }, []);
+
+  useEffect(() => {
+    if (acceptedFiles && acceptedFiles.length > 0) {
+      setFiles(acceptedFiles as FileWithPath[]);
+    }
+  }, [acceptedFiles]);
 
   console.log({ acceptedFiles, files });
 
@@ -75,23 +155,7 @@ export function ImageUploader() {
             ),
           })}
         >
-          <input
-            {...getInputProps()}
-            accept="image/*"
-            // onChange={
-            //   (e) =>
-            //     setFiles((prevFiles) => [
-            //       ...(prevFiles ?? []),
-            //       ...Array.from(e?.target?.files ?? []).filter(
-            //         (file) =>
-            //           !prevFiles?.some(
-            //             (prevFile) => prevFile.name === file.name
-            //           )
-            //       ),
-            //     ])
-            // }
-            multiple
-          />
+          <input {...getInputProps()} accept="image/*" multiple />
 
           <div className="flex flex-col items-center gap-1">
             <div className="bg-[#F8F8F8] p-4 rounded-lg">
@@ -107,37 +171,34 @@ export function ImageUploader() {
             </span>
           </div>
         </div>
+
         <aside className="mt-4">
           <h4 className="text-lg font-medium">Загруженные файлы</h4>
-          <ul className="list-disc pl-5">
-            {acceptedFiles?.map((file) => (
-              <li key={file.name}>{file.name}</li>
-            ))}
-          </ul>
+          {files.length > 0 && (
+            <FileList
+              files={files as FileWithPath[]}
+              uploading={uploading}
+              handleRemoveFile={handleRemoveFile}
+            />
+          )}
         </aside>
       </section>
 
-      <form onSubmit={handleSubmit} className="space-y-4">
-        {/* <label
-          htmlFor="file"
-          className="w-full text-sm text-slate-500
-            flex items-center justify-center
-            bg-violet-50 px-4 py-2 rounded-full
-            border-2 border-violet-200
-            hover:border-violet-400 hover:bg-violet-100
-            cursor-pointer"
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col-reverse sm:flex-row gap-3 sm:items-center"
+      >
+        <button
+          type="submit"
+          disabled={!files?.length || uploading}
+          className="w-fit text-white bg-[#1751D0] px-4 py-2 rounded-md
+                    hover:bg-blue-600 focus:ring-blue-400
+                    disabled:bg-gray-500 cursor-pointer"
         >
-          <span className="mr-2 font-bold">Выбрать изображение</span>
-          <input
-            type="file"
-            accept="image/*"
-            id="file"
-            onChange={(e) => setFile(e.target.files?.[0] || null)}
-            className="hidden"
-          />
-        </label> */}
+          {uploading ? "Загрузка..." : "Загрузить"}
+        </button>
 
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 w-fit">
           <input
             type="checkbox"
             id="private-mode"
@@ -148,42 +209,36 @@ export function ImageUploader() {
 
           <label
             htmlFor="private-mode"
-            className="cursor-pointer text-wrap !pt-0"
+            className="cursor-pointer text-nowrap !pt-0 w-full"
           >
             Приватное изображение
           </label>
 
           <button
-            className={clsx("relative max-w-52 w-full", info || "active:pt-2")}
+            className={clsx("relative", info || "active:pt-2")}
             onClick={(e) => {
               e.stopPropagation();
               e.preventDefault();
               setInfo(!info);
             }}
           >
-            <div
-              role="tooltip"
-              className={clsx(
-                "absolute -top-2 left-2 z-10 p-2 text-sm font-medium text-white transform -translate-x-1/2 -translate-y-full bg-gray-900 rounded",
-                info ? "opacity-100 visible" : "opacity-0 invisible"
-              )}
+            <Popover
+              isOpen={info}
+              positions={["top", "bottom", "left", "right"]}
+              content={
+                <div className="w-full flex justify-center">
+                  <div className="bg-gray-900 rounded p-2 w-3/4 text-balance text-center mb-2">
+                    Приватное изображение не будет отображаться в галерее
+                  </div>
+                </div>
+              }
             >
-              Приватное изображение не будет отображаться в галерее
-            </div>
-
-            <InfoIcon />
+              <div onClick={() => setInfo(!info)}>
+                <InfoIcon size={20} />
+              </div>
+            </Popover>
           </button>
         </div>
-
-        <button
-          type="submit"
-          disabled={!acceptedFiles?.length || uploading}
-          className="w-full text-white bg-violet-500 px-4 py-2 rounded-full
-                     hover:bg-violet-600 focus:outline-none focus:ring-2
-                     focus:ring-violet-400 disabled:bg-violet-900 cursor-pointer"
-        >
-          {uploading ? "Загрузка..." : "Загрузить"}
-        </button>
       </form>
 
       {viewUrl && (
