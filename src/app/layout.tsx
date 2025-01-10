08c46441e81a0b9669f86a29a8cf4a723123a7fc
akicool <liid05754@gmail.com>
2025-01-10 19:17:42 +0300

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/Toaster";
@@ -30,6 +31,20 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-J67JRXKMNX`}
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {` 
+          window.dataLayer = window.dataLayer || []; 
+          function gtag(){dataLayer.push(arguments);} 
+          gtag('js', new Date()); 
+          gtag('config', 'G-J67JRXKMNX'); 
+        `}
+        </Script>
       </body>
     </html>
   );
