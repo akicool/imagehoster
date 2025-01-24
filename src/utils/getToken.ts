c4e522876239a060c131b65df45ad2bf1004d132
akicool <liid05754@gmail.com>
2025-01-24 15:18:47 +0300

    fix: get value from cookie store

--- src/utils/getToken.ts
+++ src/utils/getToken.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export const getToken = async () => {
   const cookieStore = await cookies();
-  const token = cookieStore.get("token")?.value;
+  const token = cookieStore.get("admin")?.value;
 
   return token;
 };
