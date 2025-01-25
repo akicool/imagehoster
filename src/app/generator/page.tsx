d028baa2d35620376d89aaf3221ae92f514c9f72
akicool <liid05754@gmail.com>
2025-01-25 20:43:53 +0300

    feat: some style improvements

--- src/app/generator/page.tsx
+++ src/app/generator/page.tsx
@@ -3,7 +3,7 @@ import { Logo } from "@/shared/Logo";
 
 export default async function GeneratorPage() {
   return (
-    <main className="container mx-auto flex min-h-screen flex-col items-center justify-center px-10 py-5 sm:p-20 text-black relative gap-4">
+    <main className="container mx-auto flex w-full h-dvh flex-col items-center justify-center px-10 pt-20 pb-5 sm:p-20 text-black relative gap-4">
       <div className="absolute top-8 left-4">
         <Logo />
       </div>
