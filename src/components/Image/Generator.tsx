3262b1f1644849932cd1f0aa2b1e1fb62e238696
akicool <liid05754@gmail.com>
2025-01-31 23:21:06 +0300

    feat: add prompt generate button

--- src/components/Image/Generator.tsx
+++ src/components/Image/Generator.tsx
@@ -2,80 +2,19 @@
 
 import clsx from "clsx";
 import Image from "next/image";
-import { ChangeEvent, useState, useRef } from "react";
+import { BotIcon, Loader2Icon } from "lucide-react";
+import { ChangeEvent, useState } from "react";
 
 export default function ImageGenerator() {
   const [prompt, setPrompt] = useState("");
   const [imageUrl, setImageUrl] = useState<string>("");
   const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  // eslint-disable-next-line
-  const addWatermark = (originalImageUrl: string): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      const image = new window.Image() as HTMLImageElement;
-      image.crossOrigin = "anonymous";
-      image.src = originalImageUrl;
-
-      image.onload = () => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
-
-        const ctx = canvas.getContext("2d");
-        if (!ctx) return;
-
-        canvas.width = image.width;
-        canvas.height = image.height;
-
-        ctx.drawImage(image, 0, 0);
-
-        ctx.fillStyle = "rgba(0, 0, 0)";
-        ctx.font = "bold 26px monospace";
-        ctx.lineWidth = 4;
-        ctx.strokeStyle = "rgba(255, 255, 255, 0.8)";
-
-        const badgeText = "imagehoster.ru";
-
-        const textMetrics = ctx.measureText(badgeText);
-        const padding = 20;
-
-        const badgeX = canvas.width - textMetrics.width - padding * 2;
-        const badgeY = canvas.height - 45 - padding;
-        ctx.strokeRect(
-          badgeX,
-          badgeY,
-          textMetrics.width + padding * 2,
-          45 + padding
-        );
-
-        ctx.fillRect(
-          badgeX,
-          badgeY,
-          textMetrics.width + padding * 2,
-          45 + padding
-        );
-
-        ctx.fillStyle = "white";
-        ctx.fillText(
-          badgeText,
-          canvas.width - textMetrics.width - padding,
-          canvas.height - padding
-        );
-
-        const watermarkedImageUrl = canvas.toDataURL("image/png");
-        resolve(watermarkedImageUrl);
-      };
-
-      image.onerror = () => {
-        reject(new Error("Failed to load image"));
-      };
-    });
-  };
+  const [isLoadingImage, setIsLoadingImage] = useState(false);
+  const [isLoadingText, setIsLoadingText] = useState(false);
 
   const generateImage = async (e: React.FormEvent) => {
     e.preventDefault();
-    setIsLoading(true);
+    setIsLoadingImage(true);
     setError("");
     setImageUrl("");
 
@@ -99,7 +38,6 @@ export default function ImageGenerator() {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
-      //   const watermarkedImage = await addWatermark(response.url);
       setImageUrl(response.url);
     } catch (error) {
       console.error("Error generating image:", error);
@@ -107,28 +45,71 @@ export default function ImageGenerator() {
         error instanceof Error ? error.message : "An unknown error occurred"
       );
     } finally {
-      setIsLoading(false);
+      setIsLoadingImage(false);
+    }
+  };
+
+  const generateText = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoadingText(true);
+    const uniqueId = String(Date.now()).slice(7, String(Date.now()).length);
+    const ModelAI = "deepseek";
+
+    try {
+      const response = await fetch(`https://text.pollinations.ai`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          messages: [
+            {
+              role: "user",
+              content:
+                "Generate a text prompt in the language in which the following message will be used to generate images, leave only the text prompt.",
+            },
+            {
+              role: "user",
+              content: prompt,
+            },
+          ],
+          seed: uniqueId,
+          model: ModelAI,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const text = await response.text();
+      setPrompt(text);
+    } catch (error) {
+      console.error("Error generating text:", error);
+      setPrompt(
+        error instanceof Error ? error.message : "An unknown error occurred"
+      );
+    } finally {
+      setIsLoadingText(false);
     }
   };
 
   return (
     <div className="w-full max-w-md flex flex-col gap-4">
-      {/* <canvas ref={canvasRef} style={{ display: "none" }} /> */}
-
-      {isLoading && <div className="file-loader !w-full" />}
+      {isLoadingImage && <div className="file-loader !w-full" />}
 
       <div
         className={clsx(
           "transition duration-1000 ease-in-out overflow-hidden",
-          imageUrl && !isLoading
+          imageUrl && !isLoadingImage
             ? "max-h-full opacity-100"
             : "max-h-0 opacity-0"
         )}
         style={{
-          maxHeight: imageUrl && !isLoading ? "500px" : "0px",
-          opacity: imageUrl && !isLoading ? 1 : 0,
+          maxHeight: imageUrl && !isLoadingImage ? "500px" : "0px",
+          opacity: imageUrl && !isLoadingImage ? 1 : 0,
           transition:
-            imageUrl && !isLoading
+            imageUrl && !isLoadingImage
               ? "max-height 500ms ease-out, opacity 500ms ease-out"
               : "max-height 300ms ease-in, opacity 630ms ease-in",
         }}
@@ -154,15 +135,35 @@ export default function ImageGenerator() {
           placeholder="Введите описание изображения"
           required
           className="w-full p-2 border border-gray-300 rounded-lg h-fit text-wrap resize-none"
+          disabled={isLoadingImage || isLoadingText}
         />
 
-        <button
-          type="submit"
-          disabled={isLoading || !prompt}
-          className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:bg-gray-400"
-        >
-          {isLoading ? "Генерирую..." : "Сгенерировать изображение"}
-        </button>
+        <div className="flex gap-2 items-center">
+          <button
+            type="submit"
+            disabled={isLoadingImage || isLoadingText || !prompt}
+            className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:bg-gray-400"
+          >
+            {isLoadingImage ? "Генерирую..." : "Сгенерировать изображение"}
+          </button>
+
+          {isLoadingImage || (
+            <button
+              type="button"
+              disabled={isLoadingImage || isLoadingText || !prompt}
+              onClick={generateText}
+              className="w-fit p-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:bg-gray-400"
+            >
+              {isLoadingText ? (
+                <div className="animate-spin">
+                  <Loader2Icon />
+                </div>
+              ) : (
+                <BotIcon />
+              )}
+            </button>
+          )}
+        </div>
       </form>
 
       {error && (
